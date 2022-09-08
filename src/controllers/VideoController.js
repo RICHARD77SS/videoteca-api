3d730336e5a762771f46df20d050c3142d8587d3
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-08 15:16:33 -0300

    função de deletar e dar like

--- src/controllers/VideoController.js
+++ src/controllers/VideoController.js
@@ -1,23 +1,22 @@
-const { response } = require('express');
 const { v4: uuid } = require('uuid');
 const Video = require('../models/video');
 
 module.exports = {
-//controler para rota get
+  //controler para rota get
   async index(request, response) {
     try {
       const videos = await Video.find();
       return response.status(200).json({ videos });
     } catch (error) {
-      response.status(500).json({error: error.message})
+      response.status(500).json({ error: error.message })
     }
   },
-// controler para rota post
+  // controler para rota post
   async store(request, response) {
     const { title, link } = request.body;
 
     if (!title || !link) {
-      return response.status(400).json({error: 'Missing title or link.'})
+      return response.status(400).json({ error: 'Missing title or link.' })
     }
     const video = new Video({
       _id: uuid(),
@@ -33,22 +32,46 @@ module.exports = {
     }
   },
 
-// update video data
+  // update video data
   async update(request, response) {
     const { title, link } = request.body;
 
     if (!title && !link) {
       return response
         .status(400)
-        .json({error: "You must inform a now title or a new link"})
+        .json({ error: "You must inform a now title or a new link" })
     }
     if (title) response.video.title = title; //se tiver um novo title na response muda ele 
     if (link) response.video.link = link; //se tiver um novo link na response muda ele 
     try {
       await response.video.save();
-      return response.status(200).json({message: "Video updated successfully!"})
+      return response.status(200).json({ message: "Video updated successfully!" })
     } catch (error) {
-      response.status(500).json({ error: error.message})
+      response.status(500).json({ error: error.message })
+    }
+  },
+  // Delete video infos
+  async delete(request, response) {
+
+    try {
+      await response.video.remove();
+
+      return response.status(200).json({ message: "Video deleted successfully!" });
+    } catch (error) {
+      return response.status(500).json({ error: error.message });
+    }
+  },
+
+
+  async updateLike(request, response) {
+    response.video.liked = !response.video.liked;
+    try {
+      await response.video.save();
+      return response.status(200).json({
+        message: `video ${response.video.liked ? 'liked' : "unliked"} successfully!`
+      });
+    } catch (error) {
+      response.status(400).json({ error: error.message })
     }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ const VideoMiddleware = require('./middlewares/VideoMiddleware');
 routes.get('/', (request, response) => response.send('Hello world'));
 routes.get('/videos', VideoController.index)//acessa a funçao index em video controller e faz um get nos videos 
 routes.post('/videos', VideoController.store);
-routes.put('/video/:id', VideoMiddleware.validateId, VideoController.update);
+routes.put('/videos/:id', VideoMiddleware.validateId, VideoController.update);
+routes.delete('/videos/:id', VideoMiddleware.validateId, VideoController.delete);
+routes.patch('/videos/:id', VideoMiddleware.validateId, VideoController.updateLike);
 
 module.exports = routes
